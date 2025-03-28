b7368548202ef737cf678bd9956b1bed5775d86b
jatozy <infrastructure@jato-engineering.de>
2025-03-28 22:39:51 +0100

    Bildschirmeingabe des zu eratenden Worts implementiert

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("bitte das geheime wort eingeben");
+    let mut raetsel_wort: String = String::new();
+    let ergebnis_benutzereingabe = std::io::stdin().read_line(&mut raetsel_wort);
+    if ergebnis_benutzereingabe.is_err() {
+        println!("Es ist ein Fehler aufgetreten");
+        return;
+    }
+    raetsel_wort.pop();
+    println!("das eingegebene wort ist >{raetsel_wort}<");
 }
