3386e6a3afb42974eeaede78eaa339525227b391
jatozy <infrastructure@jato-engineering.de>
2025-04-20 11:03:22 +0200

    Test if screen output works in slice callback

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use std::error::Error;
 
 slint::include_modules!();
@@ -12,7 +9,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            let old_counter = ui.get_counter();
+            println!("Increment old_counter with old value >>{old_counter}<<");
+            ui.set_counter(old_counter + 1);
         }
     });
 
