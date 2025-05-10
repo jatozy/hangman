16b237795056a79e01623ad13309375494763a03
jatozy <infrastructure@jato-engineering.de>
2025-05-10 20:23:04 +0200

    Lerne While Schleife und Index

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,24 @@ fn main() {
     println!("bitte das geheime wort eingeben");
     let mut raetsel_wort: String = String::new();
     eingeben(&mut raetsel_wort);
+    let mut loesung: String = String::new();
+    for _ in raetsel_wort.chars() {
+        loesung.push('_');
+    }
     println!("Spieler 2 ist jetzt dran.");
-    let mut rate_buchstabe = String::new();
-    println!("rate einen Buchstaben des Wortes.");
-    eingeben(&mut rate_buchstabe);
-    for raetsel_buchstabe in raetsel_wort.chars() {
-        if raetsel_buchstabe.to_string() == rate_buchstabe {
-            println!("Der Buchstabe ist im Wort vorhanden.")
+    while loesung.contains('_') {
+        println!("Das Wort ist {loesung}");
+        let mut rate_buchstabe = String::new();
+        println!("rate einen Buchstaben des Wortes.");
+        eingeben(&mut rate_buchstabe);
+        for (position, raetsel_buchstabe) in raetsel_wort.chars().enumerate() {
+            if raetsel_buchstabe.to_string() == rate_buchstabe {
+                println!("Der Buchstabe ist im Wort vorhanden.");
+                loesung.replace_range(position..position + 1, &rate_buchstabe);
+            }
         }
     }
+    println!("Die Lösung ist {loesung}");
 }
 
 fn eingeben(resultat: &mut String) {
