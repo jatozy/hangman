5e0537660c72778d138af3eb29bdb194f0c98550
jatozy <infrastructure@jato-engineering.de>
2025-05-11 14:12:53 +0200

    Fehlerhandling hinzufügen

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,48 @@ fn main() {
     for _ in raetsel_wort.chars() {
         loesung.push('_');
     }
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
+    println!("");
     println!("Spieler 2 ist jetzt dran.");
-    while loesung.contains('_') {
+    let mut anzahl_fehler: u16 = 0;
+    while loesung.contains('_') && anzahl_fehler < 5 {
         println!("Das Wort ist {loesung}");
+        println!("Bisherige Fehler: {anzahl_fehler} von 5");
         let mut rate_buchstabe = String::new();
         println!("rate einen Buchstaben des Wortes.");
         eingeben(&mut rate_buchstabe);
-        for (position, raetsel_buchstabe) in raetsel_wort.chars().enumerate() {
-            if raetsel_buchstabe.to_string() == rate_buchstabe {
-                println!("Der Buchstabe ist im Wort vorhanden.");
-                loesung.replace_range(position..position + 1, &rate_buchstabe);
+        if raetsel_wort.contains(&rate_buchstabe) {
+            for (position, raetsel_buchstabe) in raetsel_wort.chars().enumerate() {
+                if raetsel_buchstabe.to_string() == rate_buchstabe {
+                    println!("Der Buchstabe ist im Wort vorhanden.");
+                    loesung.replace_range(position..position + 1, &rate_buchstabe);
+                }
             }
+        } else {
+            anzahl_fehler = anzahl_fehler + 1;
+            println!("Der Buchstabe ist nicht im Wort vorhanden.");
         }
     }
-    println!("Die Lösung ist {loesung}");
+    if anzahl_fehler == 5 {
+        println!("Du hast verloren");
+        println!("Das zu erratende Wort war {raetsel_wort}");
+    } else {
+        println!("Du hast gewonnen");
+        println!("Die Lösung ist {loesung}");
+    }
 }
 
 fn eingeben(resultat: &mut String) {
